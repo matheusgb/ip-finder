3c16535674b6fd16cb1d41604a057be6dc6278b5
matheusgb <matheusgbr98@gmail.com>
2022-10-04 11:02:07 -0300

    feat :sparkles: added findservers (demonstrates public servers of site)

--- app/app.go
+++ app/app.go
@@ -12,29 +12,51 @@ func Generate() *cli.App {
 	app.Name = "ip-finder"
 	app.Usage = "Busca por IPs e nomes de servidores"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "findip",
-			Usage: "Busca por IPs",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
-			Action: func(context *cli.Context) {
-				host := context.String("host")
-				ips, err := net.LookupIP(host)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for _, ip := range ips {
-					log.Println(ip)
-				}
-			},
+			Name:   "findip",
+			Usage:  "Busca por IPs",
+			Flags:  flags,
+			Action: searchIps,
+		},
+		{
+			Name:   "findservers",
+			Usage:  "Busca por nomes de servidores",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
 	return app
+}
+
+func searchIps(context *cli.Context) {
+	host := context.String("host")
+	servers, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		log.Println(server)
+	}
+}
+
+func searchServers(context *cli.Context) {
+	host := context.String("host")
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		log.Println(server)
+	}
 }
